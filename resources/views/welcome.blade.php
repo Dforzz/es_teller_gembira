89c2d12b111caca9c20a518627e556dbe73f34ea
Deedadz <deedadzahmad@gmail.com>
2026-05-13 19:27:07 +0700

    Ubah teks Dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Es Teler Gembira - Segarnya Bikin Bahagia!</title>
+    <title>Es Teler Gembira - Segarnya Bikin Gembira!</title>
     
     <!-- Meta SEO -->
     <meta name="description" content="Es Teler Gembira hadir dengan rasa premium, creamy, dan topping melimpah. Cocok untuk semua usia dengan harga bersahabat.">
